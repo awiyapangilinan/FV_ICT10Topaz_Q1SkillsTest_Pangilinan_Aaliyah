eb94860ad6009ce27bf8926adbe29328b7bf01d1
Aa <pangilinan.fs@obmontessori.edu.ph>
2026-09-21 08:41:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from pyscript import document, display
 
 def place_order(e):
     document.getElementById("output1").innerHTML = " " # clears previous result
-    prod1 = document.getElementById("item1") #get item 1 id
-    prod2 = document.getElementById("item2") #get item 2 id
-    prod3 = document.getElementById("item3") #get item 3 id
-    prod4 = document.getElementById("item4") #get item 4 id
-    prod5 = document.getElementById("item5") #get item 5 id
+    prod1 = document.getElementById("item1") 
+    prod2 = document.getElementById("item2") 
+    prod3 = document.getElementById("item3") 
+    prod4 = document.getElementById("item4") 
+    prod5 = document.getElementById("item5") 
 
     drinktotal = float(prod1.value) * prod1.checked + float(prod2.value) * prod2.checked + float(prod3.value) * prod3.checked + float(prod4.value) * prod4.checked + float(prod5.value) * prod5.checked
 
